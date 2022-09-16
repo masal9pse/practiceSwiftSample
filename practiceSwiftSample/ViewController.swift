d8c998c6f1d89fcbc17dae41f260fe9d7b76bd1c
masal9pse <returnymgstokh@icloud.com>
2022-09-16 17:36:26 +0900

    first commit

---
+++ practiceSwiftSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practiceSwiftSample
+//
+//  Created by 山本大翔 on 2022/09/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
